b4cef6c50afd4aa88b5fa6229ae60cccc9ee20b4
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2023-08-13 00:58:56 +0300

    Implemented vertices counting in wavefront file + a couple of TODOs
    This functionality is needed for allocating sufficient space for
    WavefrontGeometry class

--- WavefrontGeometry.cpp
+++ WavefrontGeometry.cpp
@@ -6,8 +6,46 @@
 #include <string>
 
 constexpr char COMMENT_MARKER = '#';
+constexpr char VERTEX_DATA_MARKER = 'v';
 
 using DataTypeIdentifier = std::string;
+
+enum class FileReadState {
+    GoodBit,
+    BadBit,
+    FailBit,
+    Eof
+};
+
+class FileReadException {
+public:
+    explicit FileReadException(FileReadState &&s) noexcept : state_(std::move(s)) {}
+
+    virtual const char *what() const noexcept {
+        switch (state_) {
+            case FileReadState::FailBit:
+                return "Filestream reported fail bit.";
+            case FileReadState::BadBit:
+                return "Filestream faced irrecoverable error.";
+            case FileReadState::Eof:
+                return "End of file was reached.";
+        }
+    }
+
+    virtual ~FileReadException();
+private:
+    const FileReadState state_;
+};
+
+class EofException : FileReadException {
+public:
+    explicit EofException() noexcept : FileReadException(FileReadState::Eof) {}
+    const char *what() const noexcept override {
+        return "Eof was reached.";
+    }
+
+    ~EofException() {}
+};
 
 struct Polygon;
 struct TextureCoordinate;
@@ -20,34 +58,71 @@ namespace wavefront {
     auto parsePolygon(const std::string_view &line) -> Polygon;
 }
 
-enum class DataType {
+enum class DataTypeId {
     Vertex,
     TextureCoordinate,
     VertexNormal,
     Polygon
 };
 
-const std::map<DataType, DataTypeIdentifier> identifiers {
-    { DataType::Vertex, "v" },
-    { DataType::TextureCoordinate, "vt" },
-    { DataType::VertexNormal, "vn" },
-    { DataType::Polygon, "f" },
+const std::map<DataTypeId, DataTypeIdentifier> identifiers {
+    { DataTypeId::Vertex, "v" },
+    { DataTypeId::TextureCoordinate, "vt" },
+    { DataTypeId::VertexNormal, "vn" },
+    { DataTypeId::Polygon, "f" }
 };
+
+// TODO: probably I should create some function which will initialize
+// WavefrontGeometry's inner memory (like allocating enough space for
+// vertices and so on), but will see.
 
 auto readWavefrontFromFile(const std::string_view &filename) -> std::shared_ptr<WavefrontGeometry> {
     std::ifstream fileStream { filename.data() };
-    std::string line;
-    while (std::getline(fileStream, line)) {
-        if (line.empty()) continue;
-        if (line.front() == COMMENT_MARKER) continue;
+    if (fileStream.is_open()) {
+        std::string line;
+        while (std::getline(fileStream, line)) {
+            if (line.empty()) continue;
+            if (line.front() == COMMENT_MARKER) continue;
 
-        std::stringstream ss { line };
+            std::istringstream ss { line };
 
-        DataTypeIdentifier identifier;
-        ss >> identifier;
-        if (identifier == identifiers.at(DataType::Vertex)) {
-            const auto v = wavefront::parseVertex(line);
+            DataTypeIdentifier identifier;
+            ss >> identifier;
+            if (identifier == identifiers.at(DataTypeId::Vertex)) {
+                const auto v = wavefront::parseVertex(line);
+                // Need to somehow implement things around memory
+                // management inside WavefrontGeometry class
+            }
+            // TODO: Parsing logic here...
         }
-        // TODO: Parsing logic here...
     }
+    else if (fileStream.bad()) throw FileReadException(FileReadState::BadBit);
+    else if (fileStream.fail()) throw FileReadException(FileReadState::FailBit);
+    else throw EofException();
+}
+
+auto countGeometryVertices(const std::string_view &filename) -> size_t {
+    std::ifstream countStream { filename.data() };
+    size_t verticesCount = 0;
+    if (countStream.is_open()) {
+        std::string line;
+        while (std::getline(countStream, line)) {
+            if (line.empty()) continue;
+            if (line.front() == COMMENT_MARKER
+             || line.front() != VERTEX_DATA_MARKER) continue;
+
+            std::istringstream ss { line };
+            std::string identifier;
+            ss >> identifier;
+            if (identifier == identifiers.at(DataTypeId::Vertex)) {
+                verticesCount++;
+            }
+        }
+        countStream.close();
+    }
+    else if (countStream.bad()) throw FileReadException(FileReadState::BadBit);
+    else if (countStream.fail()) throw FileReadException(FileReadState::FailBit);
+    else throw EofException();
+
+    return verticesCount;
 }
